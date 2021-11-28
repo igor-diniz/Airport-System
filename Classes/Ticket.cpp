18cc8506cec7ebb3ef14c48a38a35a79c18ca4e9
up202000162 <up202000162@fe.up.pt>
2021-11-28 20:35:35 +0000

    Ticket Done

--- Classes/Ticket.cpp
+++ Classes/Ticket.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "Ticket.h"
+
+Ticket::Ticket(int id, Flight flightAssocieted, Passenger passengerAssocieted){
+    this->id = id;
+    this->flightAssocieted = flightAssocieted;
+    this->passengerAssocieted = passengerAssocieted;
+}
+
+Flight Ticket::getFlightAssocited() {return flightAssocieted;}
+
+Passenger Ticket::getPassengerAssocieted() {return passengerAssocieted;}
+
+int Ticket::getID() {return id;}
+
+bool Ticket::getLuggageStatus() {return luggageIncluded;}

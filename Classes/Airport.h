de4e18f45f5235164e58cb9d1ff214034947b0cb
Eduartico <duarte.eduardoeds@gmail.com>
2021-11-28 20:04:40 +0000

    Airport stuff

--- Classes/Airport.h
+++ Classes/Airport.h
@@ -8,13 +8,18 @@
 #include "Transport.h"
 #include <string>
 
+using namespace std;
 //Para cada aeroporto, a informação sobre os locais
 //de transporte terrestre (metro, comboio, autocarro) está guardada numa árvore binária de pesquisa.
 class Airport{
-    list<Transport> Transports;
+    list<Transport> ListTransp;
     string name, initials;
-
 };
+
+
+void AddTransport(Transport transport){
+    ListTransp.push_back(transport);
+}
 
 
 #endif //TP1_AED_AIRPORT_H

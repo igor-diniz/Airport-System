499208664261245a811549ebbc5a9a5ae10084ec
ianitalo <ianitalo56@hotmail.com>
2021-11-29 20:26:34 +0000

    operator < for transport

--- Classes/Transport.cpp
+++ Classes/Transport.cpp
@@ -1,4 +1,21 @@
-//
-// Created by jeffe on 25/11/2021.
-//
 #include "Transport.h"
+
+bool Transport::operator<(const Transport &transp)
+{
+    if(this->time != transp.time)
+    {
+        return this->time < transp.time;
+    }
+    else if(this->distance != transp.distance)
+    {
+        return this->distance < transp.distance;
+    }
+    else if(this->type != transp.type)
+    {
+        return this->type < transp.type;
+    }
+    else
+    {
+        return false;
+    }
+}

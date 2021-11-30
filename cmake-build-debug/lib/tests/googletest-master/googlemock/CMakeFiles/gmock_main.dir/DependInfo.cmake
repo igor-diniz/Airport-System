d404d4738a3a62c7f79ba4b6a3aa846b8762dee8
up202000162 <up202000162@fe.up.pt>
2021-11-30 00:25:59 +0000

    1 Test implemented, but the run gave an error.

---
+++ cmake-build-debug/lib/tests/googletest-master/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/drigo/Documents/GitHub/TP1_AED/lib/tests/googletest-master/googlemock/src/gmock_main.cc" "C:/Users/drigo/Documents/GitHub/TP1_AED/cmake-build-debug/lib/tests/googletest-master/googlemock/CMakeFiles/gmock_main.dir/src/gmock_main.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/tests/googletest-master/googlemock/include"
+  "../lib/tests/googletest-master/googlemock"
+  "../lib/tests/googletest-master/googletest/include"
+  "../lib/tests/googletest-master/googletest"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/drigo/Documents/GitHub/TP1_AED/cmake-build-debug/lib/tests/googletest-master/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake"
+  "C:/Users/drigo/Documents/GitHub/TP1_AED/cmake-build-debug/lib/tests/googletest-master/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/lib/tests/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/drigo/Documents/GitHub/TP1_AED/lib/tests/googletest-master/googlemock
+# Build directory: C:/Users/drigo/Documents/GitHub/TP1_AED/cmake-build-debug/lib/tests/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")

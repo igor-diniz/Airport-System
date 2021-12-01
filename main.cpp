57389c6033e063e0d4d16f2b63891944957e182d
ianitalo <ianitalo56@hotmail.com>
2021-12-01 14:34:31 +0000

    Main Atualizada

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    std::cout << "TP1_AED" << std::endl;
+    return RUN_ALL_TESTS();
 }

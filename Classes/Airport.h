8e8824e9189d9e11e5fbfe3fd354d2ea4c6ba6ab
ianitalo <ianitalo56@hotmail.com>
2021-12-19 15:36:18 +0000

    doxygen start

--- Classes/Airport.h
+++ Classes/Airport.h
@@ -11,16 +11,51 @@ class Airport{
     BST<Transport> transports = BST<Transport>(Transport());
 public:
     Airport(){};
+    ///
+    /// \param name the Airport name
+    /// \param initials the Airport initials (PRIMARY KEY)
+    ///
     Airport(string name, string initials);
+    ///
+    /// \param transport the transport that will be added to the transport BST
+    ///
     bool addTransport(Transport transport);
+    ///
+    /// \param transport the transport that will be deleted to the transport BST
+    ///
     bool deleteTransport(Transport transport);
+    ///
+    /// returns the Airport name
+    ///
     string getName() const;
+    ///
+    /// returns the Airport Initials
+    ///
     string getInitials() const;
+    ///
+    /// return a reference to the Transport BST
+    ///
     const BST<Transport>& getTransports() const;
+    ///
+    /// \param name changes the Airport name to the parameter name
+    ///
     void setName(string name);
+    ///
+    /// \param initials  changes the Airport initials to the parameter Initials
+    ///
     void setInitials (string initials);
+    ///
+    /// \param airport checks if the Airport initials are equal to the parameter airport initials
+    ///
     bool operator==(const Airport& airport) const;
+    ///
+    /// \param airport checks if the Airport name is < then the parameter airport name
+    ///
     bool operator <(const Airport& airport) const;
+    ///
+    /// \param os
+    /// \param air Airport are show in the order name - initials
+    ///
     friend ostream& operator<<(ostream& os , const Airport& air);
 
 };
--- Classes/Date.h
+++ Classes/Date.h
@@ -11,17 +11,64 @@ private:
     int day;
 public:
     Date(){};
+    ///
+    /// \param y the Date year
+    /// \param m the Date month
+    /// \param d the Date day
+    ///
     Date(int y, int m , int d);
-    explicit Date(string date); // "YYYY/MM/DD" format
+    ///
+    /// \param date string in "YYYY/MM/DD" format
+    /// this constructor is used to get dates read from file manipulation
+    ///
+    explicit Date(string date);
+    ///
+    /// returns the Date year
+    ///
     int getYear() const;
+    ///
+    /// returns the Date month
+    ///
     int getMonth() const;
+    ///
+    /// returns the Date day
+    ///
     int getDay() const;
+    ///
+    /// \param year an integer
+    /// changes the date year to the parameter year
+    ///
     void setYear(int year);
+    ///
+    /// \param month an integer
+    /// changes the date month to the parameter month
+    ///
     void setMonth(int month);
+    ///
+    /// \param day an integer
+    /// changes the date day to the parameter day
+    ///
     void setDay(int day);
+    ///
+    /// \param d1 a Date
+    /// return true if the date d1 is >= them the this Date
+    ///
     bool operator < (const Date& d1) const;
+    ///
+    /// \param d1 a Date
+    /// return true if the date d1 is != them the this Date
+    ///
     bool operator != (const Date& d1) const;
+    ///
+    /// \param d1 a Date
+    /// return true if the date d1 is == this Date
+    ///
     bool operator == (const Date& d1) const;
+    ///
+    /// \param os
+    /// \param date a Date
+    /// the dates are show in the form year/month/day
+    /// \return
     friend ostream& operator<<(ostream& os , const Date& date);
 };
 #endif //TP1_AED_DATE_H

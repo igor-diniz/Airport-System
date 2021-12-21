4f415d51316c85805a436415d4b97cde5f1ac40a
Eduartico <duarte.eduardoeds@gmail.com>
2021-12-21 16:07:40 +0000

    app.h almost done, missing igor functions

--- Classes/App.h
+++ Classes/App.h
@@ -37,7 +37,7 @@ private:
     void readLuggageCarsFile(const string& luggageCarsFile);
     ///
     ///  \param passenger updates de passenger's ticket checkin state and sends its luggage to the flights luggage lists
-    //
+    ///
     void checkin(Passenger &passenger);
     ///
     ///  creates an airport and adds it to the general airports list
@@ -52,11 +52,17 @@ private:
     ///  and gives the option to edit if found or to creat if not found
     ///
     void airportFind();
+    ///
+    ///  \param airport allows the user to update a specific airport info
+    ///
     void updateAirport(Airport &airport);
     ///
     ///  prints the airports list, sorted and filtered by user inputs
     ///
     void showAirports();
+    ///
+    ///  prints the transport menu and asks for an option
+    ///
     void transportMenu();
     ///
     ///  \param airport creates a transport and adds it to the airport's transports list
@@ -71,6 +77,9 @@ private:
     ///  and gives the option to edit if found or to creat if not found
     ///
     void transportFind(Airport &airport);
+    ///
+    ///  \param transport allows the user to update a specific transport info
+    ///
     void updateTransport(Transport &transport, Airport &airport);
     ///
     ///  prints the airport's transports list, sorted and filtered by user inputs
@@ -89,6 +98,9 @@ private:
     ///  and gives the option to edit if found or to creat if not found
     ///
     void luggageCarFind();
+    ///
+    ///  prints the luggage car list, sorted and filtered by user inputs
+    ///
     void showLuggageFromCar();
     ///
     ///  prints the luggage car list, sorted and filtered by user inputs
@@ -107,12 +119,21 @@ private:
     ///  and gives the option to edit if found or to creat if not found
     ///
     void planeFind();
+    ///
+    ///  \param plane allows the user to update a specific plane info
+    ///
     void updatePlane(Plane &plane);
     ///
     ///  prints the plane list, sorted and filtered by user inputs
     ///
     void showPlanes();
+    ///
+    ///  prints the flight menu and asks for an option
+    ///
     void flightMenu();
+    ///
+    ///  prints the service menu and asks for an option
+    ///
     void serviceMenu();
     ///
     ///  \param plane creates a service and adds it to the plane's services to do list
@@ -143,12 +164,21 @@ private:
     ///  and gives the option to edit if found or to creat if not found
     ///
     void flightFind(Plane &plane);
+    ///
+    ///  \param flight,plane allows the user to update a specific flight info
+    ///
     void updateFlight(Flight& flight,Plane &plane);
     ///
     ///  \param plane prints the plane's services to do list, sorted and filtered by user inputs
     ///
     void showFlights(Plane &plane);
+    ///
+    /// \param initials, airport checks if the airport exists and if so, adds it to the general airport list
+    ///
     bool checkAirportExists(string initials,Airport& airport);
+    ///
+    /// \param plane takes the luggage out of a car from a flight and inserts it into a given luggage car
+    ///
     void getLuggageToCar(Plane &plane);
     ///
     ///  creates a passenger and adds it to the general passengers list
@@ -196,12 +226,29 @@ private:
     /// \param ticket allows the user to update a specific passenger's ticket info
     ///
     void updateTicket(Ticket& ticket);
-
+    ///
+    /// Saves all the files in the program
+    ///
     void saveAll();
+    ///
+    /// Saves the luggage cars file (this is a auxiliary function only)
+    ///
     void saveLuggageCars();
+    ///
+    /// Saves the airports file (this is a auxiliary function only)
+    ///
     void saveAirports();
+    ///
+    /// Saves the planes file (this is a auxiliary function only)
+    ///
     void savePlanes();
+    ///
+    /// Saves the flights file (this is a auxiliary function only)
+    ///
     void saveFlights();
+    ///
+    /// Saves the passengers file (this is a auxiliary function only)
+    ///
     void savePassengers();
 };
 
